bc16a76246c0d3e13c29a81595a14148f0960452
Bana-man <ahmadalbana98@gmail.com>
2024-04-20 16:01:19 +0700

    feat: basic IDS

--- src/Backend/web-scraper-go/ids/ids.go
+++ src/Backend/web-scraper-go/ids/ids.go
@@ -3,64 +3,108 @@ package ids
 import ( 
 	"fmt" 
 	"github.com/gocolly/colly"
-	// "log"
+	"strings"
 ) 
 	
 // Initiating maps	
 	
 //visitedNode["a"] = false jika tidak ada key "a"
-var visitedNode = make(map[string]bool)
+// var pathStack []string
+// var unvisitedPath = make(map[string][]string)
 
-var nodeDepthVisitiedAt = make(map[string]int)
+// var nodeDepthVisitiedAt = make(map[string]int)
 
-var solutionGraph = make(map[string][]string)
+// var solutionGraph = make(map[string][]string)
 
 //wholeGraph["a"]["b"] = false jika "b" belum pernah menjadi child node dari "a"
-var wholeGraph = make(map[string]map[string]bool)
+// var wholeGraph = make(map[string]map[string]bool)
 
-var baseLink string = "https://en.wikipedia.org/wiki/"
 
-func nodeVisited(title string, depth int){
-	visitedNode[title] = true
-	nodeDepthVisitiedAt[title] = depth
-}
+// func nodeVisited(title string, depth int){
+	// 	visitedNode[title] = true
+	// 	nodeDepthVisitiedAt[title] = depth
+	// }
+var childNparent = make(map[string][]string)
+var depthNode = make(map[string]int)
+var unvisitedPath []string
 
-func BFS(title string) { 
+var alrFound bool = false
 
-	// pageToScrape := baseLink + "Google"
+func IDS(inputTitle string, searchTitle string, iteration int) { 
+	var baseLink string = "https://en.wikipedia.org/wiki/"
+	pageToScrape := baseLink + inputTitle
 
 	c := colly.NewCollector()
 
-	
-	c.OnRequest(func(r *colly.Request) { 
-		fmt.Println("Visiting: ", r.URL) 
-		}) 
+
+	// c.OnRequest(func(r *colly.Request) { 
+	// 	fmt.Println("Visiting: ", r.URL) 
+	// 	}) 
 		
-	c.OnError(func(_ *colly.Response, err error) { 
-		fmt.Println("Something went wrong: ", err) 
-	}) 
+	// c.OnError(func(_ *colly.Response, err error) { 
+	// 	fmt.Println("Something went wrong: ", err) 
+	// }) 
 			
-	c.OnResponse(func(r *colly.Response) { 
-		fmt.Println("Page visited: ", r.Request.URL) 
-	}) 
-	
-	c.OnHTML("a", func(e *colly.HTMLElement) { 
-		// printing all URLs associated with the a links in the page
+	// c.OnResponse(func(r *colly.Response) { 
+	// 	if iteration == 2 {
+	// 		fmt.Println(iteration, " Page visited: ", r.Request.URL) 
+	// 	}
+	// }) 
+
+	c.OnHTML("a", func(e *colly.HTMLElement) {
 		if(isWiki(e.Attr("href"))){
-			fmt.Println(e.Attr("href")) 
-			fmt.Println(getArticleTitle(e.Attr("href")))
+			// fmt.Println(getArticleTitle(e.Attr("href")))
+			var foundTitle string = getArticleTitle(e.Attr("href"))
+			
+			val, exists := depthNode[foundTitle]
+			newVal := depthNode[inputTitle] + 1
+
+			if (val == newVal) {
+				childNparent[foundTitle] = append(childNparent[foundTitle], inputTitle)
+			} else if (!exists || val > newVal) {
+				depthNode[foundTitle] = newVal
+				childNparent[foundTitle] = []string{inputTitle}
+			}
+
+			if (foundTitle == searchTitle) {
+				alrFound = true
+			} else if (iteration > 1) && (!exists || val > newVal) {
+				IDS(foundTitle, searchTitle, iteration-1)
+			} else if (iteration == 1) {
+				unvisitedPath = append(unvisitedPath, foundTitle)
+			}
 		}
-	
 	}) 
 					
-	c.OnScraped(func(r *colly.Response) { 
-		fmt.Println(r.Request.URL, " scraped!") 
-	})
+	// c.OnScraped(func(r *colly.Response) { 
+	// 	fmt.Println(r.Request.URL, " scraped!") 
+	// })
 					
-	c.Visit("https://en.wikipedia.org/wiki/Neuroscience")
-	fmt.Println(title)
+	c.Visit(pageToScrape)
 }
 				
+func MainIDS(inputTitle string, searchTitle string, iteration int) {
+	childNparent[inputTitle] = []string{inputTitle}
+	depthNode[inputTitle] = 1
+	IDS(inputTitle, searchTitle, iteration+1)
+
+	for (!alrFound) {
+
+		for _, input := range unvisitedPath {
+			IDS(input, searchTitle, iteration)
+		}
+
+		unvisitedPath = []string{}
+	}
+
+	var a = childNparent[searchTitle]
+	fmt.Print(searchTitle, ", ")
+	for (a[0] != inputTitle) {
+		fmt.Print(a[0], ", ")
+		a = childNparent[a[0]]
+	}
+	fmt.Print(a[0])
+}
 				
 // HELPER FUNCTIONS
 func isWiki(link string) bool{
@@ -68,8 +112,11 @@ func isWiki(link string) bool{
 		// fmt.Println("Length <= 6")
 		return false
 	}else if(link[:6] == "/wiki/"){
-		// fmt.Println("Wiki link!")
-		return true;
+		if (strings.ContainsRune(link[6:], ':') || link[6:] == "Main_Page") {
+			return false
+		} else {
+			return true;
+		}
 	}else{
 		// fmt.Println("NOT Wiki link!")
 		return false;
@@ -79,77 +126,3 @@ func isWiki(link string) bool{
 func getArticleTitle(link string) string{
 	return link[6:]
 }
-
-
-// SCRAPER FUNCTIONS
-
-// // Base Web Link
-// var baseLink string = "en.wikipedia.org/wiki/"
-
-// // initializing the list of pages to scrape with an empty slice 
-// var pagesToScrape []string 
-
-// // the first pagination URL to scrape 
-// pageToScrape := "https://scrapeme.live/shop/page/1/" 
-
-// // initializing the list of pages discovered with a pageToScrape 
-// pagesDiscovered := []string{ pageToScrape } 
-
-// // current iteration 
-// i := 1 
-// // max pages to scrape 
-// limit := 5 
-
-// // initializing a Colly instance 
-// c := colly.NewCollector() 
-
-// c.OnRequest(func(r *colly.Request) { 
-// 	fmt.Println("Visiting: ", r.URL) 
-// }) 
-
-// c.OnError(func(_ *colly.Response, err error) { 
-// 	log.Println("Something went wrong: ", err) 
-// }) 
-
-// c.OnResponse(func(r *colly.Response) { 
-// 	fmt.Println("Page visited: ", r.Request.URL) 
-// }) 
-
-// // iterating over the list of pagination links to implement the crawling logic 
-// c.OnHTML("a.page-numbers", func(e *colly.HTMLElement) { 
-// 	// discovering a new page 
-// 	newPaginationLink := e.Attr("href") 
-
-// 	// if the page discovered is new 
-// 	if !contains(pagesToScrape, newPaginationLink) { 
-// 		// if the page discovered should be scraped 
-// 		if !contains(pagesDiscovered, newPaginationLink) { 
-// 			pagesToScrape = append(pagesToScrape, newPaginationLink) 
-// 		} 
-// 		pagesDiscovered = append(pagesDiscovered, newPaginationLink) 
-// 	} 
-// }) 
-
-// c.OnHTML("li.product", func(e *colly.HTMLElement) { 
-// 	// scraping logic... 
-// }) 
-
-// c.OnScraped(func(response *colly.Response) { 
-// 	// until there is still a page to scrape 
-// 	if len(pagesToScrape) != 0 && i < limit { 
-// 		// getting the current page to scrape and removing it from the list 
-// 		pageToScrape = pagesToScrape[0] 
-// 		pagesToScrape = pagesToScrape[1:] 
-
-// 		// incrementing the iteration counter 
-// 		i++ 
-
-// 		// visiting a new page 
-// 		c.Visit(pageToScrape) 
-// 	} 
-// }) 
-
-// // visiting the first page 
-// c.Visit(pageToScrape) 
-
-// // convert the data to CSV...
